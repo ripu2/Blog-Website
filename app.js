5347fd5d207186e3a785383246d70c396a42aa13
ripu <damanripu664@gmail.com>
2020-10-15 01:36:05 +0530

    Deployment ready

--- app.js
+++ app.js
@@ -67,9 +67,6 @@ app.get("/posts/:postName", (req, res) => {
 })
 
 
-
-
-
-app.listen(3000, function() {
-    console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Server started succesfully ");
 });
